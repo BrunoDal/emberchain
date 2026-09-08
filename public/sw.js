3f24d0991d540b47ff44c6d364f66180c1c53fcc
Bruno Dal <BrunoDal@users.noreply.github.com>
2026-09-08 19:53:40 +0200

    Refine chain cues and combat pacing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v11";
+const VERSION = "v12";
 const SHELL_CACHE = `emberchain-shell-${VERSION}`;
 const RUNTIME_CACHE = `emberchain-runtime-${VERSION}`;
 const scopeUrl = new URL("./", self.registration.scope);
